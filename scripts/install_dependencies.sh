0a00b5f5d1b850e43ff76306563b5be2b4853a24
iuliia <iuliiaivakina@yahoo.com>
2025-05-03 18:06:36 -0400

    updated shell script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install apache2 -y
-sudo systemctl start apache2
-sudo systemctl enable apache2
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
